97f5799233a55172cfe7ff5206830af00f972f93
Intan Malahayati <intan31@mhs.unsyiah.ac.id>
2021-05-14 14:15:54 +0700

    Mengubah file ke dalam php

---
+++ Header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    <link rel="shortcut icon" href="img/K.ico?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ </head>
+ <body>
+    <nav class="nav">
+        <a class="navbar-brand logo" href="Home.php">
+        <img src="img/K.png" width="30" height="30" class="d-inline-block align-top" style="margin-left: 20px;">
+        Keuchik</a>
+    </nav>
