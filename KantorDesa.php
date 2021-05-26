bc343fdc624d75866c433094d89277ffd43e7f74
Intan Malahayati <intan31@mhs.unsyiah.ac.id>
2021-05-26 14:26:23 +0700

    Membuat backend halaman login

--- KantorDesa.php
+++ KantorDesa.php
@@ -1,22 +1,24 @@
 <?php
-
 require ('functions.php');
 
-if(isset($_POST["login"])){
-  $nip=$_POST["nip"];
-  $password=$_POST["password"];
+if( isset($_POST["login"]) ) {
+
+  $nip = $_POST["nip"];
+  $password = $_POST["password"];
 
   $result = mysqli_query($conn, "SELECT * FROM kantordesa WHERE NIP = '$nip'");
+  $row = mysqli_fetch_assoc($result);
 
-  if(mysqli_num_rows($result)===1){
-    $row=mysqli_fetch_assoc($result);
-    
-    if(password_verify($password, $row["password"])){
+  if( mysqli_num_rows($result) === 1 ) {
+    if($password == $row["Pass"]) {
       header("Location: Pemberitahuan.php");
       exit;
     }
+    else{
+      $error = true;
+    }
   }
-  $error=true;
+  $error = true;
 }
 ?>
 
@@ -31,13 +33,13 @@ if(isset($_POST["login"])){
     <div id="login-row" class="row justify-content-center align-items-center">
       <div id="login-column" class="col-md-6">
         <div id="login-box" class="col-md-12">
-
-        <?php if( isset($error) ) : ?>
-        <p style="color: red; font-size: 12px;">Nomor Induk Pegawai / Password salah</p>
-        <?php endif; ?>
-
-          <form id="login-form" class="form" action="Pemberitahuan.php" method="POST">
+          <form id="login-form" class="form" action="" method="POST">
             <h3 class="text-center text-dark">Login</h3>
+            
+            <?php if( isset($error) ) : ?>
+            <p style="color: red; font-size: 12px;">Nomor Induk Pegawai / Password salah</p>
+            <?php endif; ?>
+            
             <div class="form-group">
               <label for="nip" class="text-dark">NIP</label><br>
               <input type="text" name="nip" id="nip" class="form-control">
@@ -47,7 +49,6 @@ if(isset($_POST["login"])){
               <input type="password" name="password" id="password" class="form-control">
             </div>
             <div class="form-group">
-              <label for="remember-me" class="text-dark"><span><input id="remember-me" name="remember-me" type="checkbox"></span><span> Remember me</span></label><br>
               <button type="submit" class="btn btn-secondary" name="login">Login</button>
             </div>
           </form>
--- Warga.php
+++ Warga.php
@@ -1,24 +1,28 @@
 <?php
 require ('functions.php');
 
-if(isset($_POST["login"])){
-  $nik=$_POST["nik"];
-  $password=$_POST["password"];
+if( isset($_POST["login"]) ) {
+
+  $nik = $_POST["nik"];
+  $password = $_POST["password"];
 
   $result = mysqli_query($conn, "SELECT * FROM warga WHERE NIK = '$nik'");
-
-  if(mysqli_num_rows($result)===1){
-    $row=mysqli_fetch_assoc($result);
-    
-    if(password_verify($password, $row['password'])){
-      echo 'ok';
+  $row = mysqli_fetch_assoc($result);
+  
+  if( mysqli_num_rows($result) === 1 ) {
+    if($password == $row["pass"]) {
       header("Location: FormWarga.php");
-      exit();
+      exit;
+    }
+    else{
+      $error = true;
     }
   }
-  $error=true;
+  $error = true;
 }
+
 ?>
+
 <?php include ('Header.php'); ?>
 <title>Login warga | Keuchik</title>
 
@@ -30,27 +34,28 @@ if(isset($_POST["login"])){
     <div id="login-row" class="row justify-content-center align-items-center">
       <div id="login-column" class="col-md-6">
         <div id="login-box" class="col-md-12">
-
-        <?php if( isset($error) ) : ?>
-        <p style="color: red; font-size: 12px;">Nomor Induk Kependudukan / Password salah</p>
-        <?php endif; ?>
-
-          <form id="login-form" class="form" action="FormWarga.php" method="POST">
+          <form id="login-form" class="form" action="" method="POST">
             <h3 class="text-center text-dark">Login</h3>
+            
+            <?php if( isset($error) ) : ?>
+            <p style="color: #d63333; font-size: 14px;">NIK / Password salah!</p>
+            <?php endif; ?>
+
             <div class="form-group">
               <label for="nik" class="text-dark">NIK</label><br>
-              <input type="text" name="nik" id="nik" class="form-control">
+              <input type="text" name="nik" id="nik" class="form-control" required />
             </div>
             <div class="form-group">
               <label for="password" class="text-dark">Password</label><br>
-              <input type="password" name="password" id="password" class="form-control">
+              <input type="password" name="password" id="password" class="form-control" required />
             </div>
             <div class="form-group">
-              <label for="remember-me" class="text-dark"><span>
-              <input id="remember-me" name="remember-me" type="checkbox"></span><span> Remember me</span></label><br>
-              <button type="submit" class="btn btn-secondary">Login</button>
+              <button type="submit" class="btn btn-secondary" name="login">Login</button>
             </div>
           </form>
+          <p class="login-card-footer-text text-dark">Belum terdaftar? 
+          <a class="text-dark" href="Registrasi.php">Daftar disini!</a>
+          </p>
         </div>
       </div>
     </div>
