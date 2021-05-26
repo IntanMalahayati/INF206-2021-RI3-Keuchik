019af7c46c7fb2ed5e6a922c17b3d41cdea50cd3
Luthfi Hanif <luthfi26@mhs.unsyiah.ac.id>
2021-05-26 23:18:03 +0700

    Membuat Koneksi Form ke database

--- FormWarga.php
+++ FormWarga.php
@@ -1,7 +1,7 @@
 <?php include ('Header.php'); ?>
 <title>Form Pengajuan | Keuchik</title>
 
-<form>
+<form action="" method="post">
   <div class="container">
     <div class="row">
       <div class="col">
@@ -14,30 +14,30 @@
     <div class="row">
         <div class="col-sm-6 bordered">
             <div class="form-group">
-                <label for="">Nama Lengkap</label>
-                <input type="text" class="form-control" id="">
+                <label for="">Nama</label>
+                <input type="text" class="form-control" name="namalengkap" id="">
             </div>
             <div class="form-group">
                 <label for="">NIK</label>
-                <input type="text" class="form-control" id="">
+                <input type="text" class="form-control" name="nik" id="">
             </div>
             <div class="form-group">
                 <label for="">Usia</label>
-                <input type="text" class="form-control" id="">
+                <input type="text" class="form-control" name="usia" id="">
             </div>
         </div>
         <div class="col-sm-6 bordered">
             <div class="form-group">
                 <label for="">Alamat</label>
-                <input type="text" class="form-control" id="">
+                <input type="text" class="form-control" name="alamat" id="">
             </div>
             <div class="form-group">
                 <label for="">Email</label>
-                <input type="text" class="form-control" id="">
+                <input type="text" class="form-control" name="email" id="">
             </div>
             <div class="form-group">
                 <label for="">No HP</label>
-                <input type="text" class="form-control" id="">
+                <input type="text" class="form-control" name="nohp" id="">
             </div>
         </div>
     </div>
@@ -50,39 +50,67 @@
     <div class="row">
         <div class="col-sm-6 bordered">
             <div class="form-group">
-                <label for="">Nama Kegiatan</label>
-                <input type="text" class="form-control" id="">
+                <label for="">Kegiatan</label>
+                <input type="text" class="form-control" name="kegiatan" id="">
             </div>
             <div class="form-group">
                 <label for="">Dalam Rangka</label>
-                <input type="text" class="form-control" id="">
+                <input type="text" class="form-control" name="dalamrangka" id="">
             </div>            
             <div class="form-group">
                 <label for="">Hari/Tanggal</label>
-                <input type="text" class="form-control" id="">
+                <input type="date" class="form-control" name="haritanggal" id="">
             </div>
             <div class="form-group">
                 <label for="">Waktu</label>
-                <input type="text" class="form-control" id="">
+                <input type="time" class="form-control" name="waktu" id="">
             </div>
         </div>
             <div class="col-sm-6 bordered">
             <div class="form-group">
                 <label for="">Tempat</label>
-                <input type="text" class="form-control" id="">
+                <input type="text" class="form-control" name="tempat" id="">
             </div>
             <div class="form-group">
                 <label for="">Penanggung Jawab</label>
-                <input type="text" class="form-control" id="">
+                <input type="text" class="form-control" name="penanggungjawab" id="">
             </div>
             <div class="form-group">
                 <label for="">Keterangan</label>
-                <input type="text" class="form-control" style="height: 121px;" id="">
+                <input type="text" class="form-control" name="keterangan" style="height: 121px;" id="">
             </div>
         </div>
     </div>
-    <p style="text-align: right;"><button type="button" class="btn btn-dark form-group" style="text-align:right;">Submit</button></p>
+    <p style="text-align: right;"><button type="submit" class="btn btn-dark form-group" name="proses" style="text-align:right;">Submit</button></p>
   </div>
 </form>
 
-<?php include ('Footer.php'); ?>
+<?php include ('Footer.php'); 
+
+?>
+
+<?php
+
+include "koneksi.php";
+
+if(isset($_POST['proses'])){
+    mysqli_query($koneksi, "insert into datawarga set
+    namalengkap = '$_POST[namalengkap]',
+    nik = '$_POST[nik]',
+    usia = '$_POST[usia]',
+    alamat = '$_POST[alamat]',
+    email = '$_POST[email]',
+    nohp = '$_POST[nohp]',
+    kegiatan = '$_POST[kegiatan]',
+    dalamrangka = '$_POST[dalamrangka]',
+    haritanggal = '$_POST[haritanggal]',
+    waktu = '$_POST[waktu]',
+    tempat = '$_POST[tempat]',
+    penanggungjawab = '$_POST[penanggungjawab]',
+    keterangan = '$_POST[keterangan]'");
+
+    echo "Data telah dikirim!";
+}
+
+
+?>
---
+++ koneksi.php
@@ -0,0 +1,5 @@
+<?php
+
+$koneksi = mysqli_connect("localhost", "root", "", "WebsitePengajuan", null, 8080);
+
+?>
